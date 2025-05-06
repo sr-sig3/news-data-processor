a1ca9ecbd8a0f42118471e89f0eef1728d1c70c3
stellakang <kangsj123@naver.com>
2025-05-06 21:36:22 +0900

    fix(process): modify server ip and loggers

--- src/consumer.py
+++ src/consumer.py
@@ -9,13 +9,37 @@ logger = structlog.get_logger()
 
 class NewsConsumer:
     def __init__(self, bootstrap_servers: str, topic: str, group_id: str):
-        self.consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers=bootstrap_servers,
-            group_id=group_id,
-            auto_offset_reset='earliest',
-            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-        )
+        logger.info("initializing_consumer",
+                   bootstrap_servers=bootstrap_servers,
+                   topic=topic,
+                   group_id=group_id)
+        
+        try:
+            self.consumer = KafkaConsumer(
+                bootstrap_servers=bootstrap_servers,
+                group_id=group_id,
+                auto_offset_reset='earliest',
+                enable_auto_commit=True,
+                value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+            )
+            logger.info("consumer_initialized_successfully")
+            
+            # 토픽 목록 확인
+            topics = self.consumer.topics()
+            logger.info("available_topics", topics=list(topics))
+            
+            # 파티션 정보 확인
+            partitions = self.consumer.partitions_for_topic(topic)
+            logger.info("topic_partitions", partitions=partitions)
+            
+            # 토픽 구독
+            self.consumer.subscribe([topic])
+            logger.info("subscribed_to_topic", topic=topic)
+            
+        except Exception as e:
+            logger.error("consumer_initialization_failed", error=str(e))
+            raise
+            
         self.db = Database()
         self.keyword_extractor = KeywordExtractor()
         
@@ -49,6 +73,10 @@ class NewsConsumer:
         
         try:
             for message in self.consumer:
+                logger.info("received_message",
+                          topic=message.topic,
+                          partition=message.partition,
+                          offset=message.offset)
                 self.process_message(message.value)
                 
         except KeyboardInterrupt:
@@ -63,9 +91,14 @@ if __name__ == "__main__":
     
     load_dotenv()
     
-    BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
-    TOPIC = os.getenv('KAFKA_TOPIC', 'news')
+    BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'news-stream-broker-kafka-1:9092')
+    TOPIC = os.getenv('KAFKA_TOPIC', 'news-topic')
     GROUP_ID = os.getenv('KAFKA_GROUP_ID', 'news-processor')
+    
+    logger.info("starting_application",
+                bootstrap_servers=BOOTSTRAP_SERVERS,
+                topic=TOPIC,
+                group_id=GROUP_ID)
     
     consumer = NewsConsumer(BOOTSTRAP_SERVERS, TOPIC, GROUP_ID)
     consumer.consume() 
